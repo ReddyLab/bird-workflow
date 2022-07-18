d63c7e3132ae710ecfc5c13eacd05c3cd3422756
Brendan Kearney <btk20@duke.edu>
2022-07-18 13:57:53 -0400

    updated with comments

--- bird_Docker_build/run_BIRD.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-bird.py \
-BIRD-1.1 \
-1.25 \
-count_files/sczps_merged_counts_for_bird_no_header.txt \
-stan_files/${NAME}_stan_${NUMBER}.txt \
-1000 \
-${RANGE} > BIRD_outputs/outputs/sczps_no_peaks_BIRD_output_${NUMBER}.txt
