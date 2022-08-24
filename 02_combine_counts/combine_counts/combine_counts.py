de5b657492be6ae6117a1f8d3515e8f3b077f3f6
Thomas Cowart <thomas.cowart@duke.edu>
2022-08-24 16:12:48 -0400

    Clean up combine_counts.py some
    * Rename "merge_counts" to the more accurate "read_csvs"
    * turn "read_csvs" into an iterator, so not all files have to be
      in memory at once
    * Move "input_count" and "output_count" insertion until after the filtering,
      so the filtering can be greatly simplified
    * combine all concats into a single concat

--- 02_combine_counts/combine_counts/combine_counts.py
+++ 02_combine_counts/combine_counts/combine_counts.py
@@ -5,48 +5,40 @@ import sys
 import pandas as pd
 
 
-def merge_counts(count_files):
-    base_names = [os.path.basename(file_name) for file_name in count_files]
-    return [
-        pd.read_csv(
+def read_csvs(count_files):
+    for file_name in count_files:
+        yield pd.read_csv(
             file_name,
             sep="\t",
             index_col=0,
             names=[
-                base_name.replace("_counts.txt", ""),
-                base_name.replace("ref_counts.txt", "alt"),
+                os.path.basename(file_name).replace("_counts.txt", ""),
+                os.path.basename(file_name).replace("ref_counts.txt", "alt"),
             ],
         )
-        for file_name, base_name in zip(count_files, base_names)
-    ]
 
 
 def run(args):
     inputs = sorted(args.inputs)
     outputs = sorted(args.outputs)
-    input_df = pd.concat(merge_counts(inputs), axis=1).sort_index()
-    output_df = pd.concat(merge_counts(outputs), axis=1).sort_index()
 
-    input_df.insert(0, "input_count", len(inputs))
-    output_df.insert(0, "output_count", len(outputs))
-    df = pd.concat([input_df, output_df], axis=1).fillna(0).astype(int)
+    df = pd.concat(read_csvs(inputs + outputs), axis=1).fillna(0).astype(int)
 
-    # Filter any positions where there are no input reads or output reads
-    # I suspect there's a better way to do this.
-    low_count_filter = pd.concat(
-        [
-            df[input_df.columns].agg("sum", axis=1) > 0,
-            df[output_df.columns].agg("sum", axis=1) > 0,
-        ],
-        axis=1,
-    ).all(axis="columns")
-    df[low_count_filter].to_csv(sys.stdout, sep="\t", header=False)
+    # Filter out low count sites
+    df = df[df.agg("sum", axis=1) > 0]
+
+    df.insert(0, "input_count", len(inputs))
+    df.insert(len(inputs) * 2 + 1, "output_count", len(outputs))
+
+    df.to_csv(sys.stdout, sep="\t", header=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Merge counts for BIRD processing")
     parser.add_argument("--inputs", "-i", nargs="+", help="Input replicate file names")
-    parser.add_argument("--outputs", "-o", nargs="+", help="Output replicate file names")
+    parser.add_argument(
+        "--outputs", "-o", nargs="+", help="Output replicate file names"
+    )
 
     args = parser.parse_args()
     run(args)
